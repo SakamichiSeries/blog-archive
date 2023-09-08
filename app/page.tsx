6cf4cf7d293b6abb1cf6854930a52e05ed92e5a6
CY <iamCristYE@Gmail.com>
2023-09-08 22:17:47 +0800

    web ver

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,127 @@
+"use client"
+
 import Image from 'next/image'
+import { useState, useEffect } from 'react'
+
+
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+    const [currentIndex, setCurrentIndex] = useState(0)
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    let setIndex = (x: number) => {
+        console.log("currentIndex", x)
+        setCurrentIndex(x)
+    }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    useEffect(() => {
+        fetch('/hayakawa_seira.json')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data)
+                setLoading(false)
+            })
+    }, [])
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+    if (isLoading) return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            Loading
+        </main>
+    )
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+
+    return (
+        <main className="flex min-h-screen md:flex-row lg:flex-col items-center justify-between h-screen bg-purple-100 flex-wrap">
+            {/* <div className='w-full h-24 p-9 bg-white font-semibold flex-grow flex-shrink basis-1'>
+        Unofficial mirror of {data.member_name_romaji.toUpperCase()}'s blog
+      </div> */}
+            <div className='grid grid-cols-12 w-full h-full'>
+                <div className="hidden md:block md:col-span-6 lg:col-span-4 overflow-auto h-full p-6 divide-y divide-gray-300">
+                    {data.blog.map((blog, index) =>
+                        <div className='p-1' key={index} onClick={() => setIndex(index)}>
+                            <div className="hover:bg-white hover:cursor-pointer rounded-3xl">
+                                <div className='p-3 pb-0'>{blog.title}</div>
+                                <div className='p-3 pt-0 text-gray-600'>{blog.time}</div>
+                            </div>
+
+                            {/* <hr></hr> */}
+                        </div>
+                    )}
+                </div>
+                <div className="col-span-12 md:order-1 md:col-span-6 h-full overflow-auto p-6 bg-white">
+                    <div>
+                        <div className="font-semibold py-3 text-4xl text-purple-900">
+                            {data.blog[currentIndex].title}
+                        </div>
+                        <div className='font-light pb-12 text-gray-600'>
+                            {data.blog[currentIndex].time}
+                        </div>
+
+                    </div>
+                    <div dangerouslySetInnerHTML={{ __html: data.blog[currentIndex].content }}>
+                    </div>
+
+                    <div className='pt-12'>
+
+                        Original link: <a className="underline" href={data.blog[currentIndex].url}>{data.blog[currentIndex].url}</a>
+                    </div>
+                </div>
+
+                <div className="hidden md:flex md:-order-1 md:col-span-6 lg:order-2 lg:col-span-2 p-9 flex-col h-screen overflow-y-auto">
+                    <div className='min-h-fit p-1'>
+                        {/* <Image alt={data.member_name} src={data.profile_pic} fill={true} style={{
+                            objectFit: 'contain',
+                        }}>
+
+                        </Image> */}
+                        <img alt={data.member_name} src={data.profile_pic} className='max-h-48'></img>
+                    </div>
+                    <div className='p-1 pt-3 font-semibold text-purple-900 text-2xl'>{data.member_name}</div>
+
+                    {/* <div className='p-1'>
+                        {data.member_name_kana}</div>
+                    <div className='p-1'>
+                        {data.member_name_romaji}</div> */}
+                    <div className='p-1'>
+                        {data.生年月日}</div>
+                    <div className='p-1'>
+                        {data.血液型}</div>
+                    <div className='p-1'>
+                        {data.星座}
+                    </div>
+                    <div className='p-1'>
+                        {data.身長}</div>
+                    <div className='p-1'>
+
+                        <span>
+                            {Object.keys(data.SNS).map((keyname) =>
+                                <a key={keyname} href={data.SNS[keyname]}>
+                                    {keyname}
+                                </a>
+                            )}
+                        </span>
+
+
+                    </div>
+
+                    <br></br>
+                    <br></br>
+
+                    <div className='p-1 flex-1'>
+                        <span className=''>
+                            This site is an unofficial mirror of {data.member_name_romaji.toUpperCase()}'s blog.
+                        </span>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </main>
+    )
 }
