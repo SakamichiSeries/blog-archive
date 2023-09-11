d870cff468a2eb243ab6a34df08eeb11e9f2839f
CY <iamCristYE@Gmail.com>
2023-09-11 12:57:07 +0800

    `the first working version`

--- app/hayakawa-seira/page.tsx
+++ app/hayakawa-seira/page.tsx
@@ -1,24 +1,33 @@
 "use client"
 
-import Image from 'next/image'
 import { useState, useEffect } from 'react'
-
+import { useRef } from 'react';
 
 
 
 export default function Home() {
-    const [data, setData] = useState(null)
+    const [data, setData] = useState<any>({})
     const [isLoading, setLoading] = useState(true)
+    const [isNavOpen, setNavOpen] = useState(false);
     const [currentIndex, setCurrentIndex] = useState(0)
 
+    const blog_main = useRef<HTMLDivElement>(null);
+    // https://stackabuse.com/bytes/fixing-the-useref-object-is-possibly-null-error-in-react/
+    // https://stackoverflow.com/questions/55677600/typescript-how-to-pass-object-is-possibly-null-error
+    const scrollBlogTop = () => {
+        if (blog_main.current !== null) {
+            // Now TypeScript knows that myRef.current is not null
+            console.log(blog_main.current.scroll({ top: 0 }));
+        }
+    }
 
-    let setIndex = (x: number) => {
+    const setIndex = (x: number) => {
         console.log("currentIndex", x)
         setCurrentIndex(x)
     }
 
     useEffect(() => {
-        fetch('/hayakawa_seira.json')
+        fetch('https://iamcristye.github.io/hayakawa-seira-blog-archive/result.json')
             .then((res) => res.json())
             .then((data) => {
                 setData(data)
@@ -26,22 +35,33 @@ export default function Home() {
             })
     }, [])
 
-    if (isLoading) return (
+    if (isLoading || blog_main.current !== null) return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             Loading
         </main>
     )
 
-
     return (
-        <main className="flex min-h-screen md:flex-row lg:flex-col items-center justify-between h-screen bg-purple-100 flex-wrap">
+        <main className="flex min-h-screen items-center justify-between h-screen w-screen bg-purple-100 flex-wrap">
             {/* <div className='w-full h-24 p-9 bg-white font-semibold flex-grow flex-shrink basis-1'>
         Unofficial mirror of {data.member_name_romaji.toUpperCase()}'s blog
       </div> */}
-            <div className='grid grid-cols-12 w-full h-full'>
-                <div className="hidden md:block md:col-span-6 lg:col-span-4 overflow-auto h-full p-6 divide-y divide-gray-300">
-                    {data.blog.map((blog, index) =>
-                        <div className='p-1' key={index} onClick={() => setIndex(index)}>
+
+            <nav className={"max-lg:flex hidden h-0 bg-white w-full min-w-full max-w-full fixed top-0"}>
+                {/* https://www.codementor.io/@giorgiasambrotta/hamburger-menu-with-react-and-tailwind-css-1qx6sruvua */}
+                <div
+                    className="menu-icon space-y-2 p-6 ml-auto mr-0 max-w-xs"
+                    onClick={() => setNavOpen((prev) => !prev)} // toggle isNavOpen state on click
+                >
+                    <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                    <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                    <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+                </div>
+            </nav>
+            <div className='flex flex-wrap flex-row w-full h-full'>
+                <div className={(isNavOpen ? "basis-3/5" : "max-lg:hidden") + " flex flex-col lg:basis-1/3 overflow-auto h-full p-6 max-lg:p-1 divide-y divide-gray-300"}>
+                    {data.blog.map((blog: any, index: number) =>
+                        <div className='px-1 py-2' key={index} onClick={() => { setIndex(index); setNavOpen(false); scrollBlogTop(); }}>
                             <div className="hover:bg-white hover:cursor-pointer rounded-3xl">
                                 <div className='p-3 pb-0'>{blog.title}</div>
                                 <div className='p-3 pt-0 text-gray-600'>{blog.time}</div>
@@ -51,9 +71,9 @@ export default function Home() {
                         </div>
                     )}
                 </div>
-                <div className="col-span-12 md:order-1 md:col-span-6 h-full overflow-auto p-6 bg-white">
+                <div ref={blog_main} className={(isNavOpen ? "max-lg:hidden" : "") + " max-lg:basis-full max-lg:max-w-full lg:basis-1/2 h-full overflow-auto p-6 bg-white"}>
                     <div>
-                        <div className="font-semibold py-3 text-4xl text-purple-900">
+                        <div className="font-semibold py-3 max-lg:pt-12 text-4xl text-purple-900">
                             {data.blog[currentIndex].title}
                         </div>
                         <div className='font-light pb-12 text-gray-600'>
@@ -65,12 +85,11 @@ export default function Home() {
                     </div>
 
                     <div className='pt-12'>
-
-                        Original link: <a className="underline" href={data.blog[currentIndex].url}>{data.blog[currentIndex].url}</a>
+                        <a className="underline" href={data.blog[currentIndex].url}>Original link</a>
                     </div>
                 </div>
 
-                <div className="hidden md:flex md:-order-1 md:col-span-6 lg:order-2 lg:col-span-2 p-9 flex-col h-screen overflow-y-auto">
+                <div className={(isNavOpen ? "basis-2/5" : "max-lg:hidden") + " lg:flex lg:basis-1/6 max-lg:pt-24 p-9 max-lg:p-4 flex-col h-screen overflow-y-auto"}>
                     <div className='min-h-fit p-1'>
                         {/* <Image alt={data.member_name} src={data.profile_pic} fill={true} style={{
                             objectFit: 'contain',
@@ -79,8 +98,7 @@ export default function Home() {
                         </Image> */}
                         <img alt={data.member_name} src={data.profile_pic} className='max-h-48'></img>
                     </div>
-                    <div className='p-1 pt-3 font-semibold text-purple-900 text-2xl'>{data.member_name}</div>
-
+                    <div className='p-1 pt-3 font-semibold text-purple-900 text-2xl'>{data.member_name_kanji}</div>
                     {/* <div className='p-1'>
                         {data.member_name_kana}</div>
                     <div className='p-1'>
@@ -112,7 +130,7 @@ export default function Home() {
 
                     <div className='p-1 flex-1'>
                         <span className=''>
-                            This site is an unofficial mirror of {data.member_name_romaji.toUpperCase()}'s blog.
+                            This site is an unofficial mirror of {data.member_name_romaji.toUpperCase()}&apos;s blog.
                         </span>
 
                     </div>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    distDir: 'dist',
+    output: 'export',
+    // https://nextjs.org/docs/app/api-reference/next-config-js/assetPrefix
+    assetPrefix: "https://iamCristYe.GitHub.io/n46-blog-frontend/"
+}
 
 module.exports = nextConfig
