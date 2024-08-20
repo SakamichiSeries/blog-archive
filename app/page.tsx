2c30658af88a3c29458357501e6bcc622e3054b4
CY <iamCristYE@Gmail.com>
2024-08-20 11:52:25 +0800

    List

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,36 @@
-"use client"
-
+"use client";
 
 export default function Home() {
+    const names = [
+        { name: "Kitagawa Yuri", url: "/blog-archive/kitagawa-yuri" },
+        { name: "Hayakawa Seira", url: "/blog-archive/hayakawa-seira" },
+        { name: "Yamashita Mizuki", url: "/blog-archive/yamashita-mizuki" },
+    ];
+
+    const archiveLink = {
+        name: "Before 2024",
+        url: "https://janelin612.github.io/n46-crawler/",
+    };
+
     return (
         <main>
             <div className="flex flex-col items-start space-y-2 p-12 bg-gray-100 rounded-lg shadow-md m-auto">
-                <a href="/blog-archive/kitagawa-yuri" className="text-blue-600 hover:text-blue-800 font-semibold">Kitagawa Yuri</a>
-                <a href="/blog-archive/hayakawa-seira" className="text-blue-600 hover:text-blue-800 font-semibold">Hayakawa Seira</a>
-                <a href="/blog-archive/yamashita-mizuki" className="text-blue-600 hover:text-blue-800 font-semibold">Yamashita Mizuki</a>
-                <a href="https://janelin612.github.io/n46-crawler/" className="text-blue-600 hover:text-blue-800 font-semibold">
-                    Before 2024
+                {names.map((member, index) => (
+                    <a
+                        key={index}
+                        href={member.url}
+                        className="text-blue-600 hover:text-blue-800 font-semibold"
+                    >
+                        {member.name}
+                    </a>
+                ))}
+                <a
+                    href={archiveLink.url}
+                    className="text-blue-600 hover:text-blue-800 font-semibold"
+                >
+                    {archiveLink.name}
                 </a>
             </div>
         </main>
-    )
+    );
 }
